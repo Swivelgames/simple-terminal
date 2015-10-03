908b3032a7ba6de7977dc3fac2e3f4e7bf806dfb
Joseph Dalrymple <joseph.dalrymple@bluelogicteam.com>
2015-10-03 02:58:22 -0500

    Added JSON.stringify to `eval` function to print things nicer

--- statics/js/terminal/commands/eval.js
+++ statics/js/terminal/commands/eval.js
@@ -1,6 +1,6 @@
 define(function(){
 	return function(){
-		console.log((new Function("return "+this.argsv.slice(1).join(" "))).apply(this));
+		console.log( JSON.stringify( (new Function("return "+this.argsv.slice(1).join(" "))).apply(this)) );
 		return this.exit(0);
 	};
 });
