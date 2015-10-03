ddad9d4f6fcd7c25982529b51e9bf1797501b75b
Joseph Dalrymple <joseph.dalrymple@bluelogicteam.com>
2015-10-03 03:41:49 -0500

    Session now pulls from location information

--- statics/js/terminal/session.js
+++ statics/js/terminal/session.js
@@ -2,14 +2,14 @@ var Session = (function(){
 	var Constructor = function(user, hostname, pwd){
 		this.user = user || this.user;
 		this.hostname = hostname || this.hostname;
-		this.pwd = pwd || "/home/"+this.user;
+		this.pwd = pwd || window.location.pathname || "/home/"+this.user;
 		this.history = [];
 	};
 
 	Constructor.prototype = {
-		user: 'user',
-		hostname: 'local',
-		pwd: '~',
+		user: (window.location.hash || '#user').substr(1),
+		hostname: (window.location.hostname || 'local'),
+		pwd: null,
 		history: null,
 		elevated: false,
 		sudoLecture: false
